ba8339a9fb6de60c5cdbd8909c093218bda2701f
Nadav Peled <nadav.peled@post.runi.ac.il>
2024-09-17 12:26:32 +0300

    final version with explanations

--- legoBlocks.py
+++ legoBlocks.py
@@ -15,55 +15,46 @@ import sys
 #  2. INTEGER m
 
 
-def legoBlocks(n, m):
-    # 1. we find all possible options to reach m-length row using breaks of lengths 1 to 4
-    # 2. find all possible permutations of stacking n rows, solid and unsolid
-    # 3. subtract the number of invalid permutations from total permutations
-    # 4. return the result modulo 10^9 + 7
+MOD = 1000000007
 
-    # 1. find all possible options to reach m-length row using breaks of lengths 1 to 4
-    # dp[i] = dp[i-1] + dp[i-2] + dp[i-3] + dp[i-4]
+def legoBlocks(n, m):
+    # Step 1: Find all possible options to reach m-length row using bricks of lengths 1 to 4
     dp = [0] * (m + 1)
     dp[0] = 1
     for i in range(1, m + 1):
-        for j in range(1, 5):
-            if i - j >= 0:
-                dp[i] += dp[i - j]
-                dp[i] %= 1000000007
+        for length in range(1, 5):
+            if i >= length:
+                dp[i] += dp[i - length]
+                dp[i] %= MOD
 
-    # 2. find all possible permutations of stacking n rows, solid and unsolid
-    # dp2[i] = dp[i] ** n
+    # Step 2: Find all possible permutations of stacking n rows, solid and unsolid
     dp2 = [0] * (m + 1)
     dp2[0] = 1
     for i in range(1, m + 1):
-        dp2[i] = pow(dp[i], n, 1000000007)
+        dp2[i] = pow(dp[i], n, MOD)
 
-    # 3. subtract the number of invalid permutations from total permutations
-    # dp3[i] = dp2[i] - sum(dp3[j] * dp2[i-j] for j in range(1, i))
+    # Step 3: Subtract the number of invalid permutations from total permutations
     dp3 = [0] * (m + 1)
     dp3[0] = 1
     for i in range(1, m + 1):
+        dp3[i] = dp2[i]
         for j in range(1, i):
-            dp3[i] += dp3[j] * dp2[i - j]
-            dp3[i] %= 1000000007
-        dp3[i] = (dp2[i] - dp3[i]) % 1000000007
+            dp3[i] -= dp3[j] * dp2[i - j]
+            dp3[i] %= MOD
 
+    # Step 4: Return the result modulo 10^9 + 7
     return dp3[m]
-    
-    
-    
-    
+
 
 if __name__ == '__main__':
+    import os
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     t = int(input().strip())
 
     for t_itr in range(t):
         first_multiple_input = input().rstrip().split()
-
         n = int(first_multiple_input[0])
-
         m = int(first_multiple_input[1])
 
         result = legoBlocks(n, m)
